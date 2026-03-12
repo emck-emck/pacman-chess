1e537c83585b69899012052d64cb0847d5f75a28
ericmckenzie <eric.c.mckenzie@gmail.com>
2026-03-11 22:12:00 -0400

    new colour scheme and cleaner piece logic

--- src/component/game/piece/piece-component.ts
+++ src/component/game/piece/piece-component.ts
@@ -13,37 +13,19 @@ export class PieceComponent {
 
   get displayPiece(): string {
     if (this.piece) {
-      switch (this.piece.colour) {
-        case 'white':
-          switch (this.piece.type) {
-            case 'pawn':
-              return '♙';
-            case 'rook':
-              return '♖';
-            case 'knight':
-              return '♘';
-            case 'bishop':
-              return '♗';
-            case 'queen':
-              return '♕';
-            case 'king':
-              return '♔';
-          }
-        case 'black':
-          switch (this.piece.type) {
-            case 'pawn':
-              return '♟';
-            case 'rook':
-              return '♜';
-            case 'knight':
-              return '♞';
-            case 'bishop':
-              return '♝';
-            case 'queen':
-              return '♛';
-            case 'king':
-              return '♚';
-          }
+      switch (this.piece.type) {
+        case 'pawn':
+          return '♟';
+        case 'rook':
+          return '♜';
+        case 'knight':
+          return '♞';
+        case 'bishop':
+          return '♝';
+        case 'queen':
+          return '♛';
+        case 'king':
+          return '♚';
       }
     }
     return '';
